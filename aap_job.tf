871d210b74355d70e795e256c0d5f7b0fa198ff3
Mark <mark@workstation9.oppies.homelab>
2025-12-04 15:37:57 +0100

    indentation fuckup

--- aap_job.tf
+++ aap_job.tf
@@ -11,7 +11,7 @@ data "aap_job_template" "deploy_website" {
 
 resource "aap_job" "deploy_webserver" {
   job_template_id = data.aap_job_template.deploy_webserver.id
-  inventory_id = data.aap_inventory.TechXchangeNL.id
+  inventory_id = aap_inventory.TechXchangeNL.id
   organization_name = "TechXchangeNL"
   depends_on = [
     aap_host.my_host,
@@ -21,7 +21,7 @@ resource "aap_job" "deploy_webserver" {
 
 resource "aap_job" "deploy_website" {
   job_template_id = data.aap_job_template.deploy_website.id
-  inventory_id = data.aap_inventory.TechXchangeNL.id
+  inventory_id = aap_inventory.TechXchangeNL.id
   organization_name = "TechXchangeNL"
   depends_on = [
     aap_host.my_host,
