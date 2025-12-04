e2771989c7b87af2c5c28756d53241a5161dab05
Mark <mark@workstation9.oppies.homelab>
2025-12-04 15:28:37 +0100

    added provider

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">= 1.0.0"
-  
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -25,4 +25,15 @@ terraform {
       source = "ansible/aap"
     }
   }
+}
+
+provider "aap" {
+  host = "https://https://caap.fvz.ansible-labs.de/"
+
+  # Token authentication is recommended
+  token = "zFaqyV8ta7uxMOWgcdSZ6FyWIrDUl9" # Also supports AAP_TOKEN environment variable
+
+  # Basic authentication is also supported, ignored if token is set
+  username = "mark.oppelaar@hcs-company.com" # Also supports AAP_USERNAME environment variable
+  password = "T3chXchang3N1" # Also supports AAP_PASSWORD environment variable
 }
